955d52f95c68de92f31c0f84829ee3a5e0be7939
Simon Boivin <sfp.boivin@gmail.com>
2021-09-02 12:14:27 +0200

    Ajout Gestion des erreurs

--- src/main/java/fr/sboivin/springdemo/controllers/VillesController.java
+++ src/main/java/fr/sboivin/springdemo/controllers/VillesController.java
@@ -2,12 +2,14 @@ package fr.sboivin.springdemo.controllers;
 
 import fr.sboivin.springdemo.entities.Ville;
 import fr.sboivin.springdemo.repositories.VilleRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -63,7 +65,7 @@ public class VillesController {
             model.addAttribute("value_cp", v.getCodePostal());
             model.addAttribute("button_submit_text", "Mettre à jour");
         } catch (Exception e) {
-            System.out.println(e);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La ville " + id + " n'est pas trouvée");
         }
         return "villes/add_edit";
     }
@@ -76,17 +78,21 @@ public class VillesController {
             v.setCodePostal(Integer.parseInt(request.getParameter("cp")));
             vr.save(v);
         } catch (Exception e) {
-            System.out.println(e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erreur dans l'édition de la ville "+id);
         }
         return "redirect:/villes/list";
     }
 
     @GetMapping(value = "/delete/{id}")
     public String deleteVilleGet(Model model, @PathVariable int id) {
-        model.addAttribute("entete_titre", "Supprimer Ville ID " + String.valueOf(id));
-        Ville v = vr.findById(id).orElse(null);
-        model.addAttribute("confirmation_text", "La ville " + v.getNom()+" va être supprimer");
-        model.addAttribute("button_submit_text", "Supprimer");
+        try {
+            model.addAttribute("entete_titre", "Supprimer Ville ID " + String.valueOf(id));
+            Ville v = vr.findById(id).orElse(null);
+            model.addAttribute("confirmation_text", "La ville " + v.getNom() + " va être supprimer");
+            model.addAttribute("button_submit_text", "Supprimer");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La ville " + id + " n'est pas trouvée");
+        }
         return "common/delete";
     }
 
@@ -97,7 +103,7 @@ public class VillesController {
             Ville v = vr.findById(id).orElse(null);
             vr.delete(v);
         } catch (Exception e) {
-            System.out.println(e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erreur dans la suppression de la ville "+id);
         }
         return "redirect:/villes/list";
     }
