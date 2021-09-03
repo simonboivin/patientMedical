4f3937a28f99d9039829b95ead6c2362386cea53
Simon Boivin <sfp.boivin@gmail.com>
2021-09-03 17:22:18 +0200

    ajout personnalisation entete droite en fonction de l'utilisateur

--- src/main/java/fr/sboivin/springdemo/controllers/GlobalControllerAdvice.java
+++ src/main/java/fr/sboivin/springdemo/controllers/GlobalControllerAdvice.java
@@ -1,6 +1,9 @@
 package fr.sboivin.springdemo.controllers;
 
 
+import fr.sboivin.springdemo.entities.User;
+import fr.sboivin.springdemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,11 +11,33 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+    @Autowired
+    private final UserRepository ur;
+
+    public GlobalControllerAdvice(UserRepository ur) {
+        this.ur = ur;
+    }
+
     @ModelAttribute("bonjour")
     public String returnBonjour(Authentication authentication) {
         if (authentication != null) {
-            return authentication.getName();
+            User u = ur.findByEmail(authentication.getName());
+            return u.getName();
         } else {
-        return "Non connecté";}
+            return "Non connecté";
+        }
+    }
+
+
+    @ModelAttribute("avatar")
+    public String avatarURL(Authentication authentication) {
+        if (authentication != null) {
+            User u = ur.findByEmail(authentication.getName());
+            System.out.println(u.getPhotouser());
+            return "/profils/"+ u.getPhotouser();
+        } else {
+            return "/img/user-defaut.png";
+        }
+
     }
 }
