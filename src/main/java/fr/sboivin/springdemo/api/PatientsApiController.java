7f5f41d470acc46dcc29e9172e5ab2c0f0dddeac
Simon Boivin <sfp.boivin@gmail.com>
2021-09-09 11:20:51 +0200

    Ajout getByID à patientAPI

--- src/main/java/fr/sboivin/springdemo/api/PatientsApiController.java
+++ src/main/java/fr/sboivin/springdemo/api/PatientsApiController.java
@@ -2,9 +2,12 @@ package fr.sboivin.springdemo.api;
 
 import fr.sboivin.springdemo.entities.Patient;
 import fr.sboivin.springdemo.services.PatientsService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ws/patients")
@@ -22,9 +25,22 @@ public class PatientsApiController {
         return patientsService.getPatientsList();
     }
 
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public Patient getPatientById(@PathVariable("id") int id){
+        Optional<Patient> patientOptional = patientsService.getPatientById(id);
+        if(patientOptional.isPresent()){
+            return patientOptional.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Le patient " + id + " n'est pas trouvé");
+        }
+    }
+
+
     @PostMapping(path = "", produces = "application/json")
     public Patient addPatient(@RequestBody Patient patient) {
         return patientsService.addPatient(patient.getNom(), patient.getPrenom(), patient.getEmail(), patient.getTelephone(), patient.getVille().getId());
     }
+
+
 
 }
