683b0e4c1ff680d88f758b3dd2695f530fe655ae
Simon Boivin <sfp.boivin@gmail.com>
2021-09-17 12:14:19 +0200

    Ajout fonction gnération aléatoire de password

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,19 @@
+function charAlea() {
+    let chaine = "";
+    for (let i = 0; i < aleaBetween(10, 20); i++) {
+        let caractere = aleaBetween(48, 90);
+        while (caractere > 57 & caractere < 65) { caractere = aleaBetween(48, 90); }
+        chaine += String.fromCharCode(caractere);
+    }
+    return chaine;
+}
+
+function aleaBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function generatePassword() {
+    let passwordInput = document.getElementById("password_input");
+    passwordInput.value = charAlea();
+}
