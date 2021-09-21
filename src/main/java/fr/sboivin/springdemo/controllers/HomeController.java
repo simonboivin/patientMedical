56d5c818b93efbcc9e3da62bea4e11d71942d1a8
Simon Boivin <sfp.boivin@gmail.com>
2021-09-21 17:09:30 +0200

    Ajout compte du nombre de rendez-vous mensuels

--- src/main/java/fr/sboivin/springdemo/controllers/HomeController.java
+++ src/main/java/fr/sboivin/springdemo/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package fr.sboivin.springdemo.controllers;
 
+import fr.sboivin.springdemo.services.RendezvousService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
+    private final RendezvousService rendezvousService;
+
+    public HomeController(RendezvousService rendezvousService) {
+        this.rendezvousService = rendezvousService;
+    }
+
     @GetMapping
-    public String homePage() {
+    public String homePage(Model model) {
+        System.out.println(rendezvousService.compteRendezVousMensuels());
+        model.addAttribute("compte", rendezvousService.compteRendezVousMensuels());
         return "index";
     }
 
